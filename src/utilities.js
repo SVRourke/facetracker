4b534a53e74bd406c3134dd32f5f0e02f1cab2f9
Samuel Rourke <samuelvanrourke@gmail.com>
2021-11-07 21:39:51 -0500

    added some coordinate geometry functions: slope, distance, segment ratio, midpoint

--- src/utilities.js
+++ src/utilities.js
@@ -30,12 +30,23 @@ export const drawMesh = (predictions, ctx) => {
   if (predictions.length > 0) {
     const { bridge, left, right, center, bottom1, bottom2 } =
       processPredictions(predictions);
-    console.log(bridge, left, right, center, bottom1, bottom2);
+    const midlip = midpoint(bottom1, bottom2);
+    // console.log(distance(bridge, center), midlip);
+    console.log(segmentRatio(left, center, right));
+
     drawPoint(bridge, ctx);
     drawPoint(left, ctx);
     drawPoint(right, ctx);
     drawPoint(center, ctx);
     drawPoint(bottom1, ctx);
+    drawPoint(midlip, ctx);
     drawPoint(bottom2, ctx);
   }
 };
+
+const slope = (a, b) => (b[1] - a[1]) / (b[0] - a[0]);
+const distance = (a, b) => Math.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2);
+const midpoint = (a, b) => [a[0] + b[0], a[1] + b[1]];
+
+const segmentRatio = (a, divisor, b) =>
+  (a[1] - divisor[1]) / (divisor[1] - b[1]);
