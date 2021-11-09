ad0dde5aed2bd6f49fe65413e59f8afaba099181
SVRourke <samuelvanrourke@gmail.com>
2021-11-09 08:31:59 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
 
 import "./App.css";
+// import { drawMesh } from "./utilities";
 import { drawMesh, calculateAngles } from "./utilities";
 
 function App() {
   // define references
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-
   // load facemesh
   const runFacemesh = async () => {
     const net = await facemesh.load({
       inputResolution: { width: 640, height: 480, scale: 0.8 },
     });
+
     setInterval(() => {
       detect(net);
     }, 100);
@@ -29,21 +31,17 @@ function App() {
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
-
       // Set video width
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
-
       // Set canvas width
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
       const face = await net.estimateFaces(video, false, false, false);
       // console.log(face);
-
       // drawing
       const ctx = canvasRef.current.getContext("2d");
       drawMesh(face, ctx);
-      console.log(calculateAngles(face));
     }
   };
 
@@ -82,5 +80,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
